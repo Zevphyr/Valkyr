c62bf8524b460933c4e8397516a6fed9e7c814b7
Joe Lee <jdlee6@buffalo.edu>
2019-05-29 21:57:18 -0400

    login/registration form

---
+++ collab_website/app/forms.py
@@ -0,0 +1,31 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=4, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo(password)])
+    submit = SubmitField('Sign Up')
+
+    # fill in after we implement database feature
+    def validate_username(self, username):
+        pass
+
+    def validate_email(self, email):
+        pass
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Log In')
+
+    # fill in after we implement database feature
+    def validate_username(self, username):
+        pass
+
+    def validate_email(self, email):
+        pass
--- collab_website/app/routes.py
+++ collab_website/app/routes.py
@@ -1,4 +1,6 @@
-from flask import render_template
+from flask import render_template, url_for, redirect
+from flask_login import logout_user
+from app.forms import RegistrationForm, LoginForm
 from app import app
 
 @app.route('/')
@@ -11,3 +13,25 @@ def index():
 """ The index() function imports a in-built function that comes with the Flask framework called render_template(). /
 This function takes a template filename and a variable list of template arguments and returns the same template, \
 but with all the placeholders in it replaced with actual values. """
+
+# TODO: set up a database for users/emails so we can further develop user login/registration
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        pass
+
+    return render_template('register.html', title='Register', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        pass
+    return render_template('login.html', title='Register', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
